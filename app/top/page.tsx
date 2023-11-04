1b8070c818cf41b5897aa7ffad8f7fac86a856e3
nagasako-tetsuya <t.nagasako@adseed.co.jp>
2023-11-04 23:36:42 +0900

    nodeのfetch動作確認

---
+++ app/top/page.tsx
@@ -0,0 +1,29 @@
+import { GetServerSideProps } from "next";
+
+interface Item {
+  id: string;
+  name: string;
+}
+
+interface Items extends Item {
+  items: Item[];
+}
+
+const Home = async ({ items }: Items) => {
+  const fetchItems = async (): Promise<Item[]> => {
+    const response = await fetch("http://127.0.0.1:3000/items");
+    const data = await response.json();
+    return data;
+  };
+  const Items = await fetchItems();
+  return (
+    <main>
+      <div>ADPOINTS DEVELOPMENT START</div>
+      {Items.map((item) => (
+        <p>{item.name}</p>
+      ))}
+    </main>
+  );
+};
+
+export default Home;
