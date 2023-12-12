9e89de17ea0f054fbfd8756fe9ed9c82bc5f8ea9
nagasako-tetsuya <t.nagasako@adseed.co.jp>
2023-12-12 22:49:49 +0900

    questionのfetchメソッド修正

--- fetchers/fetchQuestions.ts
+++ fetchers/fetchQuestions.ts
@@ -1,8 +1,10 @@
 import { Question } from "@/types/Question";
 
-export const fetchQuestions = async (): Promise<Question[]> => {
+export const fetchQuestions = async (
+  user_id: number = 3
+): Promise<Question[]> => {
   //3のところをログインユーザーIDに変える
-  const response = await fetch("http://127.0.0.1:3000/questions/3", {
+  const response = await fetch(`http://127.0.0.1:3000/questions/${user_id}`, {
     cache: "no-store",
   });
   const data = await response.json();
