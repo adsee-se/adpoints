9011a159bbcf293bc3f806d6bc4597ae3c2f5cb1
fumiya-nagayama <f.nagayama@adseed.co.jp>
2023-12-30 03:02:14 +0900

    ファイル名変更・画像パスを一つに

--- components/atoms/HamburgerMenu.tsx
+++ components/atoms/HamburgerMenu.tsx
@@ -6,16 +6,15 @@ const HamburgerMenu1: any = styled('div')(() => ({
   backgroundColor: 'rgba(246, 161, 83, 1)',
   alignSelf: 'stretch',
   width: '100%',
+  paddingBottom: '30px',
   position: 'absolute',
   top: '100%',
   borderRadius: '0px',
   display: 'flex',
-  isolation: 'isolate',
-  flexDirection: 'row',
-  height: '590px', // コンテンツ部分がハンバーガーメニューで隠れる高さに指定
-  justifyContent: 'flex-start',
-  alignItems: 'flex-start',
-  padding: '0px',
+  flexDirection: 'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  height: '400px',
   boxSizing: 'border-box',
   visibility: 'visible',
   opacity: 1,
@@ -34,9 +33,7 @@ const Q: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '135px',
-  top: '75px',
+  position: 'relative',
 });
 
 const Home: any = styled('div')({
@@ -52,9 +49,7 @@ const Home: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '159px',
-  top: '159px',
+  position: 'relative',
 });
 
 const Q1: any = styled('div')({
@@ -70,9 +65,7 @@ const Q1: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '99px',
-  top: '220px',
+  position: 'relative',
 });
 
 const Q2: any = styled('div')({
@@ -88,9 +81,7 @@ const Q2: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '147px',
-  top: '282px',
+  position: 'relative',
 });
 
 const Box1: any = styled('div')({
@@ -106,34 +97,19 @@ const Box1: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '146px',
-  top: '346px',
+  position: 'relative',
 });
 
-const Line4: any = styled('div')({
-  border: '3px solid rgba(255, 255, 255, 1)',
-  width: '390px',
-  height: '0px',
-  position: 'absolute',
-  left: '0px',
-  top: '134px',
-});
-
-const Line5: any = styled('img')({
+const Line1: any = styled('img')({
   height: 'auto',
-  width: '183px',
-  position: 'absolute',
-  left: '103px',
-  top: '206px',
+  width: '100%',
+  position: 'relative',
 });
 
-const Line6: any = styled('img')({
+const Line2: any = styled('img')({
   height: 'auto',
-  width: '183px',
-  position: 'absolute',
-  left: '103px',
-  top: '268px',
+  width: '50%',
+  position: 'relative',
 });
 
 const Q3: any = styled('div')({
@@ -149,34 +125,32 @@ const Q3: any = styled('div')({
   textDecoration: 'none',
   lineHeight: '32px',
   textTransform: 'none',
-  position: 'absolute',
-  left: '171px',
-  top: '410px',
+  position: 'relative',
 });
 
 
-const Line7: any = styled('img')({
+const Line3: any = styled('img')({
   height: 'auto',
-  width: '183px',
-  position: 'absolute',
-  left: '103px',
-  top: '330px',
+  width: '50%',
+  position: 'relative',
 });
 
-const Line8: any = styled('img')({
+const Line4: any = styled('img')({
   height: 'auto',
-  width: '183px',
-  position: 'absolute',
-  left: '103px',
-  top: '394px',
+  width: '50%',
+  position: 'relative',
 });
 
-const Line9: any = styled('img')({
+const Line5: any = styled('img')({
   height: 'auto',
-  width: '183px',
-  position: 'absolute',
-  left: '103px',
-  top: '458px',
+  width: '50%',
+  position: 'relative',
+});
+
+const Line6: any = styled('img')({
+  height: 'auto',
+  width: '50%',
+  position: 'relative',
 });
 
 export interface HamburgerMenuProps {
@@ -188,48 +162,52 @@ const  HamburgerMenu = () => {
     <HamburgerMenu1>
       {/* TODO 以下はログインしているユーザーの名前を表示するように修正 */}
       <Q>{'タローさん'}</Q>
-      <Line4 />
-      <Line5
-        src={'assets/images/HamburgerMenu/Line_5.png'}
+      <Line1
+        src={'assets/images/HamburgerMenu/Line.png'}
         loading="lazy"
-        alt={'Line 5'}
+        alt={'Line 1'}
       />
       {/* 全てのリンクを仮でルートに設定 */}
       <a href="/">
         <Home>{'HOME'}</Home>
       </a>
-      <Line6
-        src={'assets/images/HamburgerMenu/Line_6.png'}
+      <Line2
+        src={'assets/images/HamburgerMenu/Line.png'}
         loading="lazy"
-        alt={'Line 6'}
+        alt={'Line 2'}
       />
       <a href="/">
         <Q1>{'ポイントチャージ'}</Q1>
       </a>
-      <Line7
-        src={'assets/images/HamburgerMenu/Line_7.png'}
+      <Line3
+        src={'assets/images/HamburgerMenu/Line.png'}
         loading="lazy"
-        alt={'Line 7'}
+        alt={'Line 3'}
       />
       <a href="/">
         <Q2>{'質問する'}</Q2>
       </a>
-      <Line8
-        src={'assets/images/HamburgerMenu/Line_8.png'}
+      <Line4
+        src={'assets/images/HamburgerMenu/Line.png'}
         loading="lazy"
-        alt={'Line 8'}
+        alt={'Line 4'}
       />
       <a href="/">
         <Box1>{'返信BOX'}</Box1>
       </a>
-      <Line9
-        src={'assets/images/HamburgerMenu/Line_9.png'}
+      <Line5
+        src={'assets/images/HamburgerMenu/Line.png'}
         loading="lazy"
-        alt={'Line 9'}
+        alt={'Line 5'}
       />
       <a href="/">
         <Q3>{'設定'}</Q3>
       </a>
+      <Line6
+        src={'assets/images/HamburgerMenu/Line.png'}
+        loading="lazy"
+        alt={'Line 6'}
+      />
     </HamburgerMenu1>
   );
 }
