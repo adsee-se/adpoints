6205e19a7d78c915966c3ca272db4cd7a4b79c41
fumiya-nagayama <f.nagayama@adseed.co.jp>
2024-01-01 21:02:06 +0900

    questionへの保存処理途中

---
+++ puters/putQuestions.ts
@@ -0,0 +1,20 @@
+import { LambdaApiResponse } from "@/types/LambdaApiResponse";
+import { Question } from "@/types/question";
+
+export const putQuestions = async (question: Question): Promise<LambdaApiResponse> => {
+  const response = await fetch(`http://127.0.0.1:3000/question`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(question),
+    cache: "no-store",
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: response.status,
+    body: data
+  };
+};
