6f0ece6cc79e8ea02b29c26456811f047df36c63
nagasako-tetsuya <t.nagasako@adseed.co.jp>
2024-01-09 22:44:15 +0900

    cors対応（不要かも）

--- puters/putQuestions.ts
+++ puters/putQuestions.ts
@@ -1,20 +1,23 @@
 import { LambdaApiResponse } from "@/types/LambdaApiResponse";
 import { Question } from "@/types/question";
 
-export const putQuestions = async (question: Question): Promise<LambdaApiResponse> => {
+export const putQuestions = async (
+  question: Question
+): Promise<LambdaApiResponse> => {
   const response = await fetch(`http://127.0.0.1:3000/question`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(question),
     cache: "no-store",
+    mode: "cors",
   });
 
   const data = await response.json();
 
   return {
     statusCode: response.status,
-    body: data
+    body: data,
   };
 };
