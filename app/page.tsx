66eca501f65fed883b69dfa27ed858a65951e9ff
nagasako-tetsuya <t.nagasako@adseed.co.jp>
2024-01-09 22:45:34 +0900

    topから見積もるページへのリンク追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import QuestionSticParent from "@/components/union/questionSticParent";
 import { fetchQuestions } from "@/fetchers/fetchQuestions";
 import EstimateLink from "@/components/union/estimateLink";
+import Link from "next/link";
 
 const Home = async () => {
   const questions = await fetchQuestions();
 
   return (
     <main>
-      <EstimateLink />
+      <Link href={"/estimate"}>
+        <EstimateLink />
+      </Link>
       <QuestionSticParent questions={questions} />
     </main>
   );
